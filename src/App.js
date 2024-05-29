781e7aaa08574394ee904d8bb588c3eeb45cb084
David Gogishvili <119844656+DavidGogishvili@users.noreply.github.com>
2024-05-29 17:50:25 +0400

    Add production build files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Screen from "./Screen/Screen";
 
 function App() {
   return (
     <div className="App">
-      <Screen/>
+        <Router>
+            <Routes>
+             <Route
+            path="/StickyNotes"
+            element={
+                <>
+                    <Screen/>
+                </>
+            }>
+            </Route>
+                </Routes>
+        </Router>
+
     </div>
 
   );
